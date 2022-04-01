89c8cad62680465b8ce2332b1b4c4338a3b04c78
superleeyom <635709492@qq.com>
2022-04-01 16:18:50 +0800

    :zap:完善下注释

--- src/main/java/com/leeyom/opml/app/OpmlUtils.java
+++ src/main/java/com/leeyom/opml/app/OpmlUtils.java
@@ -85,9 +85,9 @@ public class OpmlUtils {
         // header
         String githubName = System.getenv("GITHUB_NAME");
         StrBuilder readmd = new StrBuilder();
-        readmd.append("**<p align=\"center\">[My Feedly OPML](https://github.com/superleeyom/my-feed-OPML)</p>**").append(BR);
+        readmd.append("**<p align=\"center\">[My Feedly OPML](https://github.com/").append(githubName).append("/my-feed-OPML)</p>**").append(BR);
         readmd.append("====").append(BR).append(BR);
-        readmd.append(new BoldText("分享我订阅的一些 Blog 和 Newsletter，每天自动同步我 Feedly 上的订阅源，✅ 代表能正常订阅，❌ 代表暂无法订阅（对于无法订阅的 feed，会通过 Telegram Bot 提醒我更新），"))
+        readmd.append(new BoldText("分享我订阅的一些 Blog 和 Newsletter，每天自动同步我 Feedly 上的订阅源，✅ 代表能正常订阅，❌ 代表暂无法订阅（对于无法订阅的 feed，支持 Telegram Bot、Email、Server酱等推送工具提醒更新），"))
                 .append(new Link("opml 下载地址", "https://github.com/" + githubName + "/my-feed-OPML/releases/download/latest/feed.opml"))
                 .append(BR).append(BR);
         // github actions 上用的是零时区，需要转成北京时间，+8个小时
